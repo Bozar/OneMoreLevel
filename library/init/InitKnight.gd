de2dae31a463adc5e55cb33a6f32619d9af8fdc6
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-06-09 14:15:40 +0800

    Silent Knight Hall: Improve dungeon generating algorithm.

--- library/init/InitKnight.gd
+++ library/init/InitKnight.gd
@@ -35,61 +35,62 @@ func _init_wall() -> void:
 
 
 func _create_solid_wall() -> void:
-	var start_x: int = _ref_RandomNumber.get_int(0, _new_DungeonSize.MAX_X)
-	var start_y: int = _ref_RandomNumber.get_int(0, _new_DungeonSize.MAX_Y)
+	var min_start: int
+	var start_x: int
+	var start_y: int
 	var max_x: int
 	var max_y: int
 
+	var min_size: int
+	var max_size: int
+	var width: int
+	var height: int
+	var shrinked: Array
+
+	var dig: Array
+	var dig_x: int
+	var dig_y: int
+
+	# Initialize data.
+	# Start from [-1, -1] as the block will be shrinked later.
+	min_start = -1
+	start_x = _ref_RandomNumber.get_int(min_start, _new_DungeonSize.MAX_X)
+	start_y = _ref_RandomNumber.get_int(min_start, _new_DungeonSize.MAX_Y)
 	# The actual border length is from 2 to 3.
-	var min_size: int = 4
-	var max_size: int = 6
-	var width: int = _ref_RandomNumber.get_int(min_size, max_size)
-	var height: int = _ref_RandomNumber.get_int(min_size, max_size)
+	min_size = 4
+	max_size = 6
+	width = _ref_RandomNumber.get_int(min_size, max_size)
+	height = _ref_RandomNumber.get_int(min_size, max_size)
 
-	var dig_border: int
-	var exclude_x: int
-	var exclude_y: int
-
-	# Verify the starting point and size.
-	for x in range(start_x, start_x + width):
-		for y in range(start_y, start_y + height):
-			if (not _new_DungeonSize.is_inside_dungeon(x, y)) \
-					or _is_occupied(x, y):
-				return
+	# Verify the starting point and size. The block can extend over the edge of
+	# the map, but cannot overlap existing blocks.
+	if not _is_valid_block(start_x, start_y, width, height):
+		return
 
 	# Shrink the wall block in four directions by 1 grid.
-	start_x += 1
-	start_y += 1
-	max_x = start_x + width - 2
-	max_y = start_y + height - 2
+	shrinked = _shrink_block(start_x, start_y, width, height)
+	start_x = shrinked[0]
+	start_y = shrinked[1]
+	max_x = shrinked[2]
+	max_y = shrinked[3]
 
 	# Decide which grid to dig.
-	dig_border = _ref_RandomNumber.get_int(0, 5)
-	match dig_border:
-		# Top.
-		0:
-			exclude_x = _ref_RandomNumber.get_int(start_x, max_x)
-			exclude_y = start_y
-		# Right.
-		1:
-			exclude_x = max_x - 1
-			exclude_y = _ref_RandomNumber.get_int(start_y, max_y)
-		# Bottom.
-		2:
-			exclude_x = _ref_RandomNumber.get_int(start_x, max_x)
-			exclude_y = max_y - 1
-		# Left.
-		3:
-			exclude_x = start_x
-			exclude_y = _ref_RandomNumber.get_int(start_y, max_y)
-		# Do not dig.
-		4:
-			pass
+	dig = _dig_border(start_x, start_y, max_x, max_y)
+	dig_x = dig[0]
+	dig_y = dig[1]
 
+	if not _is_valid_hole(dig_x, dig_y, start_x, start_y, max_x, max_y):
+		dig_x = -1
+		dig_y = -1
+
+	# Set blueprint and dungeon board.
 	for x in range(start_x, max_x):
 		for y in range(start_y, max_y):
+			# Do not generate wall blocks outside the map.
+			if not _new_DungeonSize.is_inside_dungeon(x, y):
+				continue
 			# Every wall block might lose one grid.
-			if (x == exclude_x) and (y == exclude_y):
+			if (x == dig_x) and (y == dig_y):
 				continue
 
 			# Add wall blocks to blueprint and set dungeon board.
@@ -97,6 +98,78 @@ func _create_solid_wall() -> void:
 					_new_MainGroupTag.BUILDING, _new_SubGroupTag.WALL,
 					x, y)
 			_occupy_position(x, y)
+
+
+func _is_valid_block(start_x: int, start_y: int, width: int, height: int) \
+		-> bool:
+	for x in range(start_x, start_x + width):
+		for y in range(start_y, start_y + height):
+			if not _new_DungeonSize.is_inside_dungeon(x, y):
+				continue
+			if _is_occupied(x, y):
+				return false
+	return true
+
+
+func _shrink_block(start_x: int, start_y: int, width: int, height: int) \
+		-> Array:
+	start_x += 1
+	start_y += 1
+	var max_x: int = start_x + width - 2
+	var max_y: int = start_y + height - 2
+
+	return [start_x, start_y, max_x, max_y]
+
+
+func _dig_border(start_x: int, start_y: int, max_x: int, max_y: int) -> Array:
+	var dig_x: int
+	var dig_y: int
+	var border: int = _ref_RandomNumber.get_int(0, 5)
+
+	match border:
+		# Top.
+		0:
+			dig_x = _ref_RandomNumber.get_int(start_x, max_x)
+			dig_y = start_y
+		# Right.
+		1:
+			dig_x = max_x - 1
+			dig_y = _ref_RandomNumber.get_int(start_y, max_y)
+		# Bottom.
+		2:
+			dig_x = _ref_RandomNumber.get_int(start_x, max_x)
+			dig_y = max_y - 1
+		# Left.
+		3:
+			dig_x = start_x
+			dig_y = _ref_RandomNumber.get_int(start_y, max_y)
+		# Do not dig.
+		4:
+			dig_x = -1
+			dig_y = -1
+
+	return [dig_x, dig_y]
+
+
+func _is_valid_hole(dig_x: int, dig_y: int, \
+		start_x: int, start_y: int, max_x: int, max_y: int) -> bool:
+	if not _new_DungeonSize.is_inside_dungeon(dig_x, dig_y):
+		return false
+
+	var neighbor: Array = [
+		[dig_x - 1, dig_y],
+		[dig_x + 1, dig_y],
+		[dig_x, dig_y - 1],
+		[dig_x, dig_y + 1],
+	]
+
+	for n in neighbor:
+		if not _new_DungeonSize.is_inside_dungeon(n[0], n[1]):
+			continue
+		elif (n[0] < start_x) or (n[0] >= max_x) \
+				or (n[1] < start_y) or (n[1] >= max_y):
+			return true
+	return false
 
 
 func _init_PC() -> void:
