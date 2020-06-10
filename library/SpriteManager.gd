1c0d37208cad1f11b3cd682c9fe945b00a412686
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-06-10 10:27:05 +0800

    Attach a script to change sprite on the fly.

---
+++ library/SpriteManager.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+var _new_SpriteTypeTag := preload("res://library/SpriteTypeTag.gd")
+var _current = _new_SpriteTypeTag.DEFAULT
+
+
+func switch_sprite(type_tag: String) -> void:
+	if not has_node(type_tag):
+		return
+
+	get_node(_current).visible = false
+	get_node(type_tag).visible = true
+	_current = type_tag
