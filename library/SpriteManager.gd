41125788a47dc43e0de3c162c165741f39464eb1
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-06-11 20:49:38 +0800

    Add SpriteManager.set_color().

--- library/SpriteManager.gd
+++ library/SpriteManager.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 
 var _new_SpriteTypeTag := preload("res://library/SpriteTypeTag.gd")
-var _current = _new_SpriteTypeTag.DEFAULT
+
+var _current: String = _new_SpriteTypeTag.DEFAULT
 
 
 func switch_sprite(type_tag: String) -> void:
@@ -12,3 +13,9 @@ func switch_sprite(type_tag: String) -> void:
 	get_node(_current).visible = false
 	get_node(type_tag).visible = true
 	_current = type_tag
+
+
+# NOTE: Reset color if the sprite is removed from scene and will be recycled
+# later.
+func set_color(new_color: String) -> void:
+	get_node(_current).modulate = new_color
