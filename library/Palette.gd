02958391a4cfd00a5b93feaddbe7930bd18b520d
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-06-11 19:59:29 +0800

    Modify palette.

---
+++ library/Palette.gd
@@ -0,0 +1,10 @@
+# https://coolors.co/palettes/popular/grey
+
+
+const BACKGROUND: String = "212529"
+const DARK: String = "343a40"
+const SHADOW: String = "6c757d"
+const STANDARD: String = "adb5bd"
+const IMPORTANT: String = "ff8552"
+
+const DEBUG: String = "fe4a49"
