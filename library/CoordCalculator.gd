e96c903b60966fce1d0b29241bd25a17e11c5f2e
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-06-11 14:36:36 +0800

    Add CoordCalculator.gd.

---
+++ library/CoordCalculator.gd
@@ -0,0 +1,22 @@
+func get_range(source: Array, target: Array) -> int:
+	var delta_x: int = abs(source[0] - target[0]) as int
+	var delta_y: int = abs(source[0] - target[0]) as int
+
+	return delta_x + delta_y
+
+
+func is_inside_range(source: Array, target: Array, max_range: int) -> bool:
+	return get_range(source, target) <= max_range
+
+
+func get_neighbor(center: Array, distance: int) -> Array:
+	var x: int = center[0]
+	var y: int = center[1]
+	var neighbor: Array = [
+		[x - distance, y],
+		[x + distance, y],
+		[x, y - distance],
+		[x, y + distance],
+	]
+
+	return neighbor
