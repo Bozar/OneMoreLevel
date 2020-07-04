2186aa95f9e570da78d564f594ab85f4366580fb
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-07-04 21:07:53 +0800

    Rename: SpriteStatusTag -> ObjectStatusTag.

--- library/npc_ai/AITemplate.gd
+++ library/npc_ai/AITemplate.gd
@@ -2,6 +2,7 @@ var print_text: String setget set_print_text, get_print_text
 
 var _new_ConvertCoord := preload("res://library/ConvertCoord.gd").new()
 var _new_CoordCalculator := preload("res://library/CoordCalculator.gd").new()
+var _new_ObjectStatusTag := preload("res://library/ObjectStatusTag.gd").new()
 
 
 # Override.
--- scene/main/ObjectStatus.gd
+++ scene/main/ObjectStatus.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
 
-var _new_SpriteStatusTag := preload("res://library/SpriteStatusTag.gd").new()
+var _new_ObjectStatusTag := preload("res://library/ObjectStatusTag.gd").new()
 
 var _id_to_status: Dictionary = {}
 
 
 func _on_InitWorld_sprite_created(new_sprite: Sprite) -> void:
-	_id_to_status[new_sprite.get_instance_id()] = _new_SpriteStatusTag.DEFAULT
+	_id_to_status[new_sprite.get_instance_id()] = _new_ObjectStatusTag.DEFAULT
 
 
 func get_status(sprite: Sprite) -> String:
@@ -16,6 +16,10 @@ func get_status(sprite: Sprite) -> String:
 
 func set_status(sprite: Sprite, status: String) -> void:
 	_id_to_status[_get_id(sprite)] = status
+
+
+func verify_status(sprite: Sprite, status: String) -> bool:
+	return _id_to_status[_get_id(sprite)] == status
 
 
 func _get_id(sprite: Sprite) -> int:
