43098dfd3c8a59ef77786d2115a55175f730ef64
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-07-07 12:01:57 +0800

    Change sprites.

--- library/SpriteTypeTag.gd
+++ library/SpriteTypeTag.gd
@@ -1,12 +1,6 @@
 const DEFAULT: String = "default"
+const DEFAULT_2: String = "default_2"
+const DEFAULT_3: String = "default_3"
+
 const ACTIVE: String = "active"
 const PASSIVE: String = "passive"
-
-const ACTIVE_1: String = "active_1"
-const PASSIVE_1: String = "passive_1"
-
-const DEFAULT_2: String = "default_2"
-const ACTIVE_2: String = "active_2"
-
-const DEFAULT_3: String = "default_3"
-const ACTIVE_3: String = "active_3"
