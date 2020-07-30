d1690296373a8fd17e703064a9d386a90a187e85
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-07-30 20:25:19 +0800

    Add help text.

--- library/help_text/GeneralHelp.gd
+++ library/help_text/GeneralHelp.gd
@@ -1,1 +1,8 @@
-const TEXT: String = "General help"
+const TEXT: String = "Keybindings\n\n" \
+        + "* Move: Arrow Key.\n" \
+        + "* Wait: Space.\n" \
+        + "* Exit help: Esc.\n" \
+        + "* Exit game: C+W.\n\n" \
+        + "Symbols\n\n" \
+        + "* Danger: (!).\n" \
+        + "* Luck: (?)."
--- library/help_text/KnightHelp.gd
+++ library/help_text/KnightHelp.gd
@@ -1,1 +1,6 @@
-const TEXT: String = "Knight help"
+const TEXT: String = "Silent Knight Hall\n\n" \
+        + "* Kill captains (C) and the boss.\n" \
+        + "* The boss has 3 HP and 3 symbols (S, P & D).\n" \
+        + "* Stay away from danger zones (!).\n" \
+        + "* Roll to the other side of an alert enemy (!) " \
+        + "and backstab the weakened target (?)."
