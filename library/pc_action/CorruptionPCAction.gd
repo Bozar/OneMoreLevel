67fcafd90910558a85e506db4c8756dd5563027e
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-08-07 23:48:09 +0800

    Interact with building and trap in Corruption.

--- library/pc_action/CorruptionPCAction.gd
+++ library/pc_action/CorruptionPCAction.gd
@@ -5,9 +5,22 @@ func _init(parent_node: Node2D).(parent_node) -> void:
 	pass
 
 
+# TODO: Fill progress bar.
 func interact_with_trap() -> void:
-	print("trap")
+	_ref_CountDown.hit_bonus()
+	_remove_building_or_trap(false)
 
 
 func interact_with_building() -> void:
-	print("building")
+	_remove_building_or_trap(true)
+
+
+func _remove_building_or_trap(is_building: bool) -> void:
+	var x: int = _target_position[0]
+	var y: int = _target_position[1]
+
+	if is_building:
+		_ref_RemoveObject.remove(_new_MainGroupTag.BUILDING, x, y)
+	else:
+		_ref_RemoveObject.remove(_new_MainGroupTag.TRAP, x, y)
+	end_turn = true
