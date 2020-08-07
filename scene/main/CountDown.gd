4890ed22087e5dddbdbb22aba1f5194353c0b64c
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-08-07 23:40:50 +0800

    Minor fix.

--- scene/main/CountDown.gd
+++ scene/main/CountDown.gd
@@ -10,20 +10,6 @@ var _new_CountDownData := preload("res://library/CountDownData.gd").new()
 var _current_count: int = _new_CountDownData.MAX_COUNT
 
 
-func _on_Schedule_turn_started(current_sprite: Sprite) -> void:
-	if not current_sprite.is_in_group(_new_SubGroupTag.PC):
-		return
-
-	_current_count = _fix_overflow()
-	if _current_count < _new_CountDownData.MIN_COUNT:
-		_ref_EndGame.player_lose()
-
-
-func _on_Schedule_turn_ended(current_sprite: Sprite) -> void:
-	if current_sprite.is_in_group(_new_SubGroupTag.PC):
-		subtract_count(1)
-
-
 func get_count(fix_overflow: bool) -> int:
 	if fix_overflow:
 		return _fix_overflow()
@@ -40,6 +26,20 @@ func add_count(add: int) -> void:
 
 func subtract_count(subtract: int) -> void:
 	_current_count -= subtract
+
+
+func _on_Schedule_turn_started(current_sprite: Sprite) -> void:
+	if not current_sprite.is_in_group(_new_SubGroupTag.PC):
+		return
+
+	_current_count = _fix_overflow()
+	if _current_count < _new_CountDownData.MIN_COUNT:
+		_ref_EndGame.player_lose()
+
+
+func _on_Schedule_turn_ended(current_sprite: Sprite) -> void:
+	if current_sprite.is_in_group(_new_SubGroupTag.PC):
+		subtract_count(1)
 
 
 func _fix_overflow() -> int:
