6e31087af2b9c74789257493f52d7174db9724bb
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-08-12 11:36:58 +0800

    Add _bury_worm() to DesertAI.gd.

--- library/npc_ai/DesertAI.gd
+++ library/npc_ai/DesertAI.gd
@@ -4,10 +4,11 @@ extends "res://library/npc_ai/AITemplate.gd"
 var _spr_WormBody := preload("res://sprite/WormBody.tscn")
 var _spr_WormSpice := preload("res://sprite/WormSpice.tscn")
 var _spr_WormTail := preload("res://sprite/WormTail.tscn")
+var _spr_Treasure := preload("res://sprite/Treasure.tscn")
+var _spr_Wall := preload("res://sprite/Wall.tscn")
 
 var _new_DesertData := preload("res://library/npc_data/DesertData.gd")
 
-var _current_position: Array
 var _wait_one_turn: bool = false
 # int: Array[Sprite]
 var _id_to_worm: Dictionary = {}
@@ -18,30 +19,21 @@ func _init(parent_node: Node2D).(parent_node) -> void:
 
 
 func take_action(actor) -> void:
-	var id: int = actor.get_instance_id()
-	var save_position: Array
-
 	if not actor.is_in_group(_new_SubGroupTag.WORM_HEAD):
 		return
 
-	_set_local_var(actor)
-	_current_position = _self_pos
-	_random_walk()
+	var id: int = actor.get_instance_id()
 
+	_set_local_var(actor)
 	if not _id_to_worm.has(id):
 		_init_worm(actor)
+
+	# Try to move head.
+	_random_walk()
 	if _wait_one_turn:
 		return
-
-	for i in range(1, _id_to_worm[id].size()):
-		if not _id_to_worm[id][i]:
-			_create_body(id, i)
-			return
-		save_position = _new_ConvertCoord.vector_to_array(
-				_id_to_worm[id][i].position)
-		_ref_DungeonBoard.move_sprite(
-				_new_MainGroupTag.ACTOR, save_position, _current_position)
-		_current_position = save_position
+	# Move body.
+	_move_body(id)
 
 
 func _init_worm(head: Sprite) -> void:
@@ -54,31 +46,34 @@ func _init_worm(head: Sprite) -> void:
 	_id_to_worm[id][0] = head
 
 
-func _create_body(id: int, index: int) -> void:
+func _create_body(id: int, index: int, x: int, y: int) -> void:
 	var worm_length: int = _id_to_worm[id].size()
 	var spice_end: int = min(
 		_new_DesertData.SPICE_LEFT_END,
 		worm_length - _new_DesertData.SPICE_RIGHT_END
 	) as int
 
+	# Create tail.
 	if index == worm_length - 1:
 		_ref_CreateObject.create(
 				_spr_WormTail,
 				_new_MainGroupTag.ACTOR, _new_SubGroupTag.WORM_BODY,
-				_current_position[0], _current_position[1])
+				x, y)
+	# Create spice.
 	elif (index > _new_DesertData.SPICE_START) and (index < spice_end):
 		_ref_CreateObject.create(
 				_spr_WormSpice,
 				_new_MainGroupTag.ACTOR, _new_SubGroupTag.WORM_SPICE,
-				_current_position[0], _current_position[1])
+				x, y)
+	# Create body.
 	else:
 		_ref_CreateObject.create(
 				_spr_WormBody,
 				_new_MainGroupTag.ACTOR, _new_SubGroupTag.WORM_BODY,
-				_current_position[0], _current_position[1])
+				x, y)
 
 	_id_to_worm[id][index] = _ref_DungeonBoard.get_sprite(
-			_new_MainGroupTag.ACTOR, _current_position[0], _current_position[1])
+			_new_MainGroupTag.ACTOR, x, y)
 
 
 func _random_walk() -> void:
@@ -118,3 +113,46 @@ func _random_walk() -> void:
 
 func _is_pc_pos(x: int, y: int) -> bool:
 	return (x == _pc_pos[0]) and (y == _pc_pos[1])
+
+
+func _move_body(id: int) -> void:
+	var worm: Array = _id_to_worm[id]
+	var current_position: Array = _self_pos
+	var save_position: Array
+
+	for i in range(1, worm.size()):
+		if worm[i] == null:
+			_create_body(id, i, current_position[0], current_position[1])
+			return
+
+		save_position = _new_ConvertCoord.vector_to_array(worm[i].position)
+		_ref_DungeonBoard.move_sprite(
+				_new_MainGroupTag.ACTOR, save_position, current_position)
+		current_position = save_position
+
+
+func _bury_worm(id: int) -> void:
+	var whole_worm: Array = _id_to_worm[id]
+	var worm_position: Array
+	var __
+
+	for i in whole_worm:
+		if i == null:
+			return
+
+		worm_position = _new_ConvertCoord.vector_to_array(i.position)
+		_ref_RemoveObject.remove(
+				_new_MainGroupTag.ACTOR, worm_position[0], worm_position[1])
+
+		if _ref_RandomNumber.get_percent_chance(_new_DesertData.CREATE_SPICE):
+			_ref_CreateObject.create(
+					_spr_Treasure,
+					_new_MainGroupTag.TRAP, _new_SubGroupTag.TREASURE,
+					worm_position[0], worm_position[1])
+		else:
+			_ref_CreateObject.create(
+					_spr_Wall,
+					_new_MainGroupTag.BUILDING, _new_SubGroupTag.WALL,
+					worm_position[0], worm_position[1])
+
+	__ = _id_to_worm.erase(id)
--- library/npc_data/DesertData.gd
+++ library/npc_data/DesertData.gd
@@ -4,3 +4,5 @@ const MIN_LENGTH: int = 7
 const SPICE_START: int = 1
 const SPICE_LEFT_END: int = 5
 const SPICE_RIGHT_END: int = 2
+
+const CREATE_SPICE: int = 30
