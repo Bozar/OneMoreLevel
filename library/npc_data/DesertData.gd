9e6748ae5787aa50714ef6f67f51ed3a756612d9
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-08-12 16:27:11 +0800

    Adjust the number of spices a worm has.

--- library/npc_data/DesertData.gd
+++ library/npc_data/DesertData.gd
@@ -2,8 +2,8 @@ const MAX_LENGTH: int = 12
 const MIN_LENGTH: int = 7
 
 const SPICE_START: int = 1
-const SPICE_LEFT_END: int = 5
-const SPICE_RIGHT_END: int = 2
+const SPICE_LEFT_END: int = 6
+const SPICE_RIGHT_END: int = 3
 
 const CREATE_SPICE: int = 30
 
