060b66736fc1a22d4f99eb886966e98149087a60
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-08-25 18:28:31 +0800

    Do null check when removing an object.

--- scene/main/RemoveObject.gd
+++ scene/main/RemoveObject.gd
@@ -10,5 +10,7 @@ var _ref_DungeonBoard: Game_DungeonBoard
 func remove(main_group: String, x: int, y: int) -> void:
 	var sprite: Sprite = _ref_DungeonBoard.get_sprite(main_group, x, y)
 
+	if sprite == null:
+		return
 	emit_signal("sprite_removed", sprite, main_group, x, y)
 	sprite.queue_free()
