a92e6caf2a85a3f5dbcd22bb8b676233cd990f99
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-08-26 22:51:27 +0800

    Add more shorter continous flows.

--- library/npc_data/StyxData.gd
+++ library/npc_data/StyxData.gd
@@ -5,5 +5,5 @@ const RESTORE_TURN: int = 6
 
 const RENEW_MAP: int = 3
 const MIN_LENGTH: int = 5
-const MAX_LENGTH: int = 15
-const MAX_FLOW: int = 10
+const MAX_LENGTH: int = 10
+const MAX_FLOW: int = 20
