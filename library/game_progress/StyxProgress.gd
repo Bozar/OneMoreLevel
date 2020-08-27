3e78d88a03d46337def79dd861a8fcabb6a5af5b
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-08-27 09:25:42 +0800

    Shorten the length of flows in Styx.

--- library/game_progress/StyxProgress.gd
+++ library/game_progress/StyxProgress.gd
@@ -3,72 +3,54 @@ extends "res://library/game_progress/ProgressTemplate.gd"
 
 var _new_StyxData := preload("res://library/npc_data/StyxData.gd").new()
 
-var _sprite_type: Array
-var _object_state: Array
-
-var _flow_direction: Array = [
-	[0, 1], [0, -1], [1, 0], [-1, 0]
-]
+var _direction_sprite_state: Dictionary
 var _countdown: int = _new_StyxData.RENEW_MAP
 
 
 func _init(parent_node: Node2D).(parent_node) -> void:
-	_sprite_type = [
-		_new_SpriteTypeTag.UP,
-		_new_SpriteTypeTag.DOWN,
-		_new_SpriteTypeTag.LEFT,
-		_new_SpriteTypeTag.RIGHT,
-	]
-
-	_object_state = [
-		_new_ObjectStateTag.UP,
-		_new_ObjectStateTag.DOWN,
-		_new_ObjectStateTag.LEFT,
-		_new_ObjectStateTag.RIGHT,
-	]
+	_direction_sprite_state = {
+		0: [[0, 1], _new_SpriteTypeTag.UP, _new_ObjectStateTag.UP],
+		1: [[0, -1], _new_SpriteTypeTag.DOWN, _new_ObjectStateTag.DOWN],
+		2: [[1, 0], _new_SpriteTypeTag.RIGHT, _new_ObjectStateTag.RIGHT],
+		3: [[-1, 0], _new_SpriteTypeTag.LEFT, _new_ObjectStateTag.LEFT],
+	}
 
 
 func renew_world(_pc_x: int, _pc_y: int) -> void:
 	var flow_path: Array = []
+	var select_index: int
 
 	if _countdown == _new_StyxData.RENEW_MAP:
 		_countdown = 0
 		_randomize_ground()
 
+		select_index = _get_select_index()
 		for _i in range(_new_StyxData.MAX_FLOW):
-			flow_path.push_back(_get_flow_path())
+			flow_path.push_back(_get_flow_path(select_index))
 		for i in flow_path:
-			_rotate_sprite(i)
+			_set_flow_path(i, select_index)
 	_countdown += 1
 
 
 func _randomize_ground() -> void:
-	var select_index: int
-	var river_sprite: Sprite
+	var ground_sprite: Sprite
 
 	for i in range(_new_DungeonSize.MAX_X):
 		for j in range(_new_DungeonSize.MAX_Y):
-			river_sprite = _ref_DungeonBoard.get_sprite(
+			ground_sprite = _ref_DungeonBoard.get_sprite(
 					_new_MainGroupTag.GROUND, i, j)
-			if river_sprite == null:
+			if ground_sprite == null:
 				continue
-
-			select_index = _ref_RandomNumber.get_int(0, 4)
-			_ref_SwitchSprite.switch_sprite(
-					river_sprite, _sprite_type[select_index])
-			_ref_ObjectData.set_state(river_sprite, _object_state[select_index])
+			_rotate_sprite(ground_sprite, _get_select_index())
 
 
-func _get_flow_path() -> Array:
+func _get_flow_path(select_index: int) -> Array:
 	var x: int = _ref_RandomNumber.get_int(0, _new_DungeonSize.MAX_X)
 	var y: int = _ref_RandomNumber.get_int(0, _new_DungeonSize.MAX_Y)
-	var direction: Array = _flow_direction[_ref_RandomNumber.get_int(
-			0, _flow_direction.size())]
-	var flow_length: int = _ref_RandomNumber.get_int(
-			_new_StyxData.MIN_LENGTH, _new_StyxData.MAX_LENGTH)
+	var direction: Array = _get_direction(select_index)
 	var flow_path: Array = [[x, y]]
 
-	for _i in range(flow_length):
+	for _i in range(_new_StyxData.FLOW_LENGTH):
 		x = x + direction[0]
 		y = y + direction[1]
 		if not _new_CoordCalculator.is_inside_dungeon(x, y):
@@ -77,27 +59,35 @@ func _get_flow_path() -> Array:
 	return flow_path
 
 
-func _rotate_sprite(flow_path: Array) -> void:
+func _set_flow_path(flow_path: Array, select_index: int) -> void:
 	var ground_sprite: Sprite
-	var select_index: int
 
-	for i in range(1, flow_path.size()):
+	for i in range(0, flow_path.size()):
 		ground_sprite = _ref_DungeonBoard.get_sprite(
 				_new_MainGroupTag.GROUND, flow_path[i][0], flow_path[i][1])
 		if ground_sprite == null:
 			continue
+		_rotate_sprite(ground_sprite, select_index)
 
-		if flow_path[i][0] == flow_path[i - 1][0]:
-			if flow_path[i][1] > flow_path[i - 1][1]:
-				select_index = 0
-			else:
-				select_index = 1
-		elif flow_path[i][1] == flow_path[i - 1][1]:
-			if flow_path[i][0] > flow_path[i - 1][0]:
-				select_index = 3
-			else:
-				select_index = 2
 
-		_ref_SwitchSprite.switch_sprite(
-				ground_sprite, _sprite_type[select_index])
-		_ref_ObjectData.set_state(ground_sprite, _object_state[select_index])
+func _rotate_sprite(ground_sprite: Sprite, select_index: int) -> void:
+	_ref_SwitchSprite.switch_sprite(
+			ground_sprite, _get_sprite_tag(select_index))
+	_ref_ObjectData.set_state(
+			ground_sprite, _get_state_tag(select_index))
+
+
+func _get_select_index() -> int:
+	return _ref_RandomNumber.get_int(0, _direction_sprite_state.size())
+
+
+func _get_direction(select_index: int) -> Array:
+	return _direction_sprite_state[select_index][0]
+
+
+func _get_sprite_tag(select_index: int) -> String:
+	return _direction_sprite_state[select_index][1]
+
+
+func _get_state_tag(select_index: int) -> String:
+	return _direction_sprite_state[select_index][2]
--- library/npc_data/StyxData.gd
+++ library/npc_data/StyxData.gd
@@ -1,9 +1,8 @@
 const PC_SIGHT: int = 2
 const LIGHTHOUSE: int = 2
 const HARBOR: int = 1
-const RESTORE_TURN: int = 6
+const RESTORE_TURN: int = 7
 
 const RENEW_MAP: int = 3
-const MIN_LENGTH: int = 5
-const MAX_LENGTH: int = 10
+const FLOW_LENGTH: int = 4
 const MAX_FLOW: int = 20
