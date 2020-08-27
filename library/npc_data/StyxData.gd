4c07b5daf06aa577a06a778bc1c11a1a448278bf
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-08-27 10:30:06 +0800

    Lighthouse restores 5 turns instead of 6.

--- library/npc_data/StyxData.gd
+++ library/npc_data/StyxData.gd
@@ -1,7 +1,7 @@
 const PC_SIGHT: int = 2
 const LIGHTHOUSE: int = 2
 const HARBOR: int = 1
-const RESTORE_TURN: int = 7
+const RESTORE_TURN: int = 6
 
 const RENEW_MAP: int = 3
 const FLOW_LENGTH: int = 4
