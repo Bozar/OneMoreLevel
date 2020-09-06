0065037af10b640e002c34d24966dda77c64eb4f
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-09-06 17:27:21 +0800

    Qualified spice appears more frequently in Desert. (20% -> 25%)

--- library/npc_data/DesertData.gd
+++ library/npc_data/DesertData.gd
@@ -6,7 +6,7 @@ const SPICE_END: int = 5
 
 const CREATE_SPICE: int = 5
 const BONUS_CREATE_SPICE: int = 5
-const CREATE_ACTIVE_SPICE: int = 20
+const CREATE_ACTIVE_SPICE: int = 25
 
 const HP_TURN: int = 1
 const HP_WAIT: int = 5
