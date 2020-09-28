c2e8ab7158efebb98cea6689063824dcb1ab963b
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-09-28 11:56:40 +0800

    Change a phantom's sprite when it covers or uncovers a crystal.

--- library/npc_ai/MirrorAI.gd
+++ library/npc_ai/MirrorAI.gd
@@ -14,15 +14,37 @@ func take_action(actor: Sprite) -> void:
 
 	_set_local_var(actor)
 
-	if _new_CoordCalculator.is_inside_range(
-			_self_pos[0], _self_pos[1], _pc_pos[0], _pc_pos[1],
-			_new_MirrorData.ATTACK_RANGE):
+	var distance: int = _new_CoordCalculator.get_range(
+			_self_pos[0], _self_pos[1],
+			_pc_pos[0], _pc_pos[1])
+
+	if distance > _new_MirrorData.PHANTOM_SIGHT:
+		return
+	elif distance == _new_MirrorData.ATTACK_RANGE:
 		_attack()
-	elif _new_CoordCalculator.is_inside_range(
-			_self_pos[0], _self_pos[1], _pc_pos[0], _pc_pos[1],
-			_new_MirrorData.PHANTOM_SIGHT):
-		_approach_pc()
+	else:
+		_move()
 
 
 func _attack() -> void:
 	return
+
+
+func _move() -> void:
+	var new_position: Array
+	var trap: Sprite
+
+	trap = _ref_DungeonBoard.get_sprite(
+			_new_MainGroupTag.TRAP, _self_pos[0], _self_pos[1])
+	if trap != null:
+		_ref_SwitchSprite.switch_sprite(_self, _new_SpriteTypeTag.DEFAULT)
+		trap.visible = true
+
+	_approach_pc()
+
+	new_position = _new_ConvertCoord.vector_to_array(_self.position)
+	trap = _ref_DungeonBoard.get_sprite(
+			_new_MainGroupTag.TRAP, new_position[0], new_position[1])
+	if trap != null:
+		_ref_SwitchSprite.switch_sprite(_self, _new_SpriteTypeTag.ACTIVE)
+		trap.visible = false
