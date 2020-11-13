9c86de3556a5f6ad5ea821730e0186e1230ba91d
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-11-13 14:54:44 +0800

    Allow PC to move over dungeon border.

--- library/pc_action/BalloonPCAction.gd
+++ library/pc_action/BalloonPCAction.gd
@@ -1,9 +1,33 @@
 extends "res://library/pc_action/PCActionTemplate.gd"
 
 
+var _teleport_x: Dictionary
+var _teleport_y: Dictionary
+
+
 func _init(parent_node: Node2D).(parent_node) -> void:
-	pass
+	_teleport_x = {
+		-1: _new_DungeonSize.MAX_X - 1,
+		_new_DungeonSize.MAX_X: 0,
+	}
+	_teleport_y = {
+		-1: _new_DungeonSize.MAX_Y - 1,
+		_new_DungeonSize.MAX_Y: 0,
+	}
 
 
 func switch_sprite(_pc: Sprite) -> void:
 	pass
+
+
+func set_target_position(direction: String) -> void:
+	.set_target_position(direction)
+
+	var x: int = _target_position[0]
+	var y: int = _target_position[1]
+
+	if _teleport_x.has(x):
+		x = _teleport_x[x]
+	if _teleport_y.has(y):
+		y = _teleport_y[y]
+	_target_position = [x, y]
