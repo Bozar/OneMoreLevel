236ba078a58feddfb62eee9ea05eabf0719a400c
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-11-17 13:47:22 +0800

    Reaching a destination restores 24 turns in Balloon.

--- library/npc_data/BalloonData.gd
+++ library/npc_data/BalloonData.gd
@@ -1,6 +1,6 @@
 const MIN_DURATION: int = 2
 const MAX_DURATION: int = 5
 
-const RESTORE_TURN: int = 12
+const RESTORE_TURN: int = 25
 const MAX_TRAP: int = 5
-const MAX_WALL: int = 10
+const MAX_WALL: int = 12
