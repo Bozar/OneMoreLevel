29540ddad9e9535dace9857a026568cd330c7759
bozar42 <5583771+Bozar@users.noreply.github.com>
2021-01-08 12:20:36 +0800

    Create frogs in swamp.

--- library/game_progress/FrogProgress.gd
+++ library/game_progress/FrogProgress.gd
@@ -20,9 +20,9 @@ func _init(parent_node: Node2D).(parent_node) -> void:
 	pass
 
 
-func renew_world(_pc_x: int, _pc_y: int) -> void:
+func renew_world(pc_x: int, pc_y: int) -> void:
 	if _wave_counter == 0:
-		_create_frog()
+		_create_frog(pc_x, pc_y, false)
 		_wave_counter += 1
 
 
@@ -30,9 +30,39 @@ func remove_actor(_actor: Sprite, _x: int, _y: int) -> void:
 	print("remove actor")
 
 
-func _create_frog() -> void:
-	_ref_CreateObject.create(_spr_Frog,
-			_new_MainGroupTag.ACTOR, _new_SubGroupTag.FROG, 0, 0)
+func _create_frog(pc_x: int, pc_y: int, is_princess: bool) -> void:
+	var swamp: Array = _ref_DungeonBoard.get_sprites_by_tag(
+			_new_SubGroupTag.FLOOR)
+	var counter: int = 0
+	var tmp_index: int
+	var tmp_pos: Array
+	var frog_pos: Array = []
+
+	var max_frog: int = _new_FrogData.MAX_FROG
+	var frog_sight: int = _new_FrogData.SIGHT
+	var frog_sprite: PackedScene = _spr_Frog
+	var frog_tag: String = _new_SubGroupTag.FROG
+
+	if is_princess:
+		max_frog = 1
+		frog_sight = _new_FrogData.PRINCESS_SIGHT
+		frog_sprite = _spr_FrogPrincess
+		frog_tag = _new_SubGroupTag.FROG_PRINCESS
+
+	while counter < max_frog:
+		tmp_index = _ref_RandomNumber.get_int(counter, swamp.size())
+		tmp_pos = _new_ConvertCoord.vector_to_array(swamp[tmp_index].position)
+		if _new_CoordCalculator.is_inside_range(
+				tmp_pos[0], tmp_pos[1], pc_x, pc_y, frog_sight):
+			continue
+
+		_new_ArrayHelper.swap_element(swamp, counter, tmp_index)
+		frog_pos.push_back(tmp_pos)
+		counter += 1
+
+	for i in frog_pos:
+		_ref_CreateObject.create(frog_sprite,
+				_new_MainGroupTag.ACTOR, frog_tag, i[0], i[1])
 
 
 func _submerge_land() -> void:
