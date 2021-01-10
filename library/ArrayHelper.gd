7d46fa175da722ca702f8e0117edea15ebf69da0
bozar42 <5583771+Bozar@users.noreply.github.com>
2021-01-10 12:51:26 +0800

    Add ArrayHelper.filter_element().

--- library/ArrayHelper.gd
+++ library/ArrayHelper.gd
@@ -8,6 +8,21 @@ func random_picker(source_array: Array, num_of_element: int,
 	source_array.resize(num_of_element)
 
 
+# filter_in_func(source_array: Array, current_index: int,
+#> optional_arg: Array) -> bool
+# Returns true if we need an array element.
+func filter_element(source_array: Array, func_host: Object,
+		filter_in_func: String, optional_arg: Array) -> void:
+	var filter_in := funcref(func_host, filter_in_func)
+	var counter: int = 0
+
+	for i in range(source_array.size()):
+		if filter_in.call_func(source_array, i, optional_arg):
+			swap_element(source_array, counter, i)
+			counter += 1
+	source_array.resize(counter)
+
+
 func swap_element(source_array: Array,
 		left_index: int, right_index: int) -> void:
 	var tmp
