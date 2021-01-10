b67ee6f84ab686524064a28f677daf37417c5388
bozar42 <5583771+Bozar@users.noreply.github.com>
2021-01-10 13:39:56 +0800

    Clean up code in FrogProgress.

--- library/game_progress/FrogProgress.gd
+++ library/game_progress/FrogProgress.gd
@@ -33,36 +33,34 @@ func remove_actor(_actor: Sprite, _x: int, _y: int) -> void:
 func _create_frog(pc_x: int, pc_y: int, is_princess: bool) -> void:
 	var swamp: Array = _ref_DungeonBoard.get_sprites_by_tag(
 			_new_SubGroupTag.SWAMP)
-	var counter: int = 0
-	var tmp_index: int
-	var tmp_pos: Array
+	var neighbor: Array = _new_CoordCalculator.get_neighbor(
+			pc_x, pc_y, _new_FrogData.SIGHT, true)
 	var frog_pos: Array = []
 
 	var max_frog: int = _new_FrogData.MAX_FROG
-	var frog_sight: int = _new_FrogData.SIGHT
 	var frog_sprite: PackedScene = _spr_Frog
 	var frog_tag: String = _new_SubGroupTag.FROG
 
 	if is_princess:
 		max_frog = 1
-		frog_sight = _new_FrogData.PRINCESS_SIGHT
 		frog_sprite = _spr_FrogPrincess
 		frog_tag = _new_SubGroupTag.FROG_PRINCESS
 
-	while counter < max_frog:
-		tmp_index = _ref_RandomNumber.get_int(counter, swamp.size())
-		tmp_pos = _new_ConvertCoord.vector_to_array(swamp[tmp_index].position)
-		if _new_CoordCalculator.is_inside_range(
-				tmp_pos[0], tmp_pos[1], pc_x, pc_y, frog_sight):
-			continue
+	_new_ArrayHelper.filter_element(swamp, self, "_filter_spawn_position",
+			[neighbor])
+	_new_ArrayHelper.random_picker(swamp, max_frog, _ref_RandomNumber)
 
-		_new_ArrayHelper.swap_element(swamp, counter, tmp_index)
-		frog_pos.push_back(tmp_pos)
-		counter += 1
-
-	for i in frog_pos:
+	for i in swamp:
+		frog_pos = _new_ConvertCoord.vector_to_array(i.position)
 		_ref_CreateObject.create(frog_sprite,
-				_new_MainGroupTag.ACTOR, frog_tag, i[0], i[1])
+				_new_MainGroupTag.ACTOR, frog_tag, frog_pos[0], frog_pos[1])
+
+
+func _filter_spawn_position(source: Array, index: int, opt_arg: Array) -> bool:
+	var pos: Array = _new_ConvertCoord.vector_to_array(source[index].position)
+	var neighbor: Array = opt_arg[0]
+
+	return not pos in neighbor
 
 
 func _submerge_land() -> void:
