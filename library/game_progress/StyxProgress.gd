991dde028d729ae95e44909682b90d13f552ee20
bozar42 <5583771+Bozar@users.noreply.github.com>
2021-01-26 14:06:05 +0800

    Water flows more orderly in Styx.

--- library/game_progress/StyxProgress.gd
+++ library/game_progress/StyxProgress.gd
@@ -2,47 +2,75 @@ extends "res://library/game_progress/ProgressTemplate.gd"
 
 
 var _new_StyxData := preload("res://library/npc_data/StyxData.gd").new()
+var _new_ArrayHelper := preload("res://library/ArrayHelper.gd").new()
 
-var _direction_sprite_state: Dictionary
 var _init_world: bool = true
+var _state_to_coord: Dictionary
+var _state_to_sprite: Dictionary
 
 
 func _init(parent_node: Node2D).(parent_node) -> void:
-	_direction_sprite_state = {
-		0: [[0, 1], _new_SpriteTypeTag.UP, _new_ObjectStateTag.UP],
-		1: [[0, -1], _new_SpriteTypeTag.DOWN, _new_ObjectStateTag.DOWN],
-		2: [[1, 0], _new_SpriteTypeTag.RIGHT, _new_ObjectStateTag.RIGHT],
-		3: [[-1, 0], _new_SpriteTypeTag.LEFT, _new_ObjectStateTag.LEFT],
+	_state_to_coord = {
+		_new_ObjectStateTag.UP: [0, -1],
+		_new_ObjectStateTag.DOWN: [0, 1],
+		_new_ObjectStateTag.LEFT: [-1, 0],
+		_new_ObjectStateTag.RIGHT: [1, 0],
+	}
+	_state_to_sprite = {
+		_new_ObjectStateTag.UP: _new_SpriteTypeTag.UP,
+		_new_ObjectStateTag.DOWN: _new_SpriteTypeTag.DOWN,
+		_new_ObjectStateTag.LEFT: _new_SpriteTypeTag.LEFT,
+		_new_ObjectStateTag.RIGHT: _new_SpriteTypeTag.RIGHT,
 	}
 
 
 func renew_world(pc_x: int, pc_y: int) -> void:
 	var pc: Sprite = _ref_DungeonBoard.get_pc()
-
-	# NOTE: Enable _cast_light() later.
-	# _cast_light(pc_x, pc_y)
+	var renew: bool = true
 
 	if _init_world:
 		_init_world = false
 	elif _ref_ObjectData.verify_state(pc, _new_ObjectStateTag.ACTIVE):
 		_ref_ObjectData.set_state(pc, _new_ObjectStateTag.DEFAULT)
 	else:
-		return
-	_change_water_flow(pc_x, pc_y)
+		renew = false
+
+	if renew:
+		_change_water_flow()
+	_cast_light(pc_x, pc_y)
 
 
-func _change_water_flow(pc_x: int, pc_y: int) -> void:
-	var flow_path: Array = []
-	var select_index: int
+func _change_water_flow() -> void:
+	var ground: Array = _ref_DungeonBoard.get_sprites_by_tag(
+			_new_MainGroupTag.GROUND)
+	var valid_state: Array
+	var direction: String
+	var pos: Array
+	var x: int
+	var y: int
+	var flow: Sprite
 
-	_shuffle_rng(pc_x, pc_y)
-	_randomize_ground()
+	_new_ArrayHelper.filter_element(ground, self, "_filter_change_flow", [])
+	for i in ground:
+		if not _ref_ObjectData.verify_state(i, _new_ObjectStateTag.DEFAULT):
+			continue
+		valid_state = _state_to_coord.keys()
+		_new_ArrayHelper.random_picker(valid_state, 1, _ref_RandomNumber)
+		direction = valid_state[0]
+		_rotate_sprite(i, direction)
 
-	select_index = _get_select_index()
-	for _i in range(_new_StyxData.MAX_FLOW):
-		flow_path.push_back(_get_flow_path(select_index))
-	for i in flow_path:
-		_set_flow_path(i, select_index)
+		pos = _new_ConvertCoord.vector_to_array(i.position)
+		x = pos[0]
+		y = pos[1]
+		for _j in range(_new_StyxData.FLOW_LENGTH):
+			x += _state_to_coord[direction][0]
+			y += _state_to_coord[direction][1]
+			flow = _ref_DungeonBoard.get_sprite(_new_MainGroupTag.GROUND, x, y)
+			if (flow != null) and _ref_ObjectData.verify_state(flow,
+					_new_ObjectStateTag.DEFAULT):
+				_rotate_sprite(flow, direction)
+			else:
+				break
 
 
 func _cast_light(pc_x: int, pc_y: int) -> void:
@@ -66,67 +94,11 @@ func _cast_light(pc_x: int, pc_y: int) -> void:
 		i.modulate = new_color
 
 
-func _shuffle_rng(x: int, y: int) -> void:
-	var repeat: int = (x + y * _new_DungeonSize.MAX_X) % 10
-	_ref_RandomNumber.shuffle(repeat)
+func _rotate_sprite(ground: Sprite, direction: String) -> void:
+	_ref_ObjectData.set_state(ground, direction)
+	_ref_SwitchSprite.switch_sprite(ground, _state_to_sprite[direction])
 
 
-func _randomize_ground() -> void:
-	var ground_sprite: Sprite
-
-	for i in range(_new_DungeonSize.MAX_X):
-		for j in range(_new_DungeonSize.MAX_Y):
-			ground_sprite = _ref_DungeonBoard.get_sprite(
-					_new_MainGroupTag.GROUND, i, j)
-			if ground_sprite == null:
-				continue
-			_rotate_sprite(ground_sprite, _get_select_index())
-
-
-func _get_flow_path(select_index: int) -> Array:
-	var x: int = _ref_RandomNumber.get_int(0, _new_DungeonSize.MAX_X)
-	var y: int = _ref_RandomNumber.get_int(0, _new_DungeonSize.MAX_Y)
-	var direction: Array = _get_direction(select_index)
-	var flow_path: Array = [[x, y]]
-
-	for _i in range(_new_StyxData.FLOW_LENGTH):
-		x = x + direction[0]
-		y = y + direction[1]
-		if not _new_CoordCalculator.is_inside_dungeon(x, y):
-			break
-		flow_path.push_back([x, y])
-	return flow_path
-
-
-func _set_flow_path(flow_path: Array, select_index: int) -> void:
-	var ground_sprite: Sprite
-
-	for i in range(0, flow_path.size()):
-		ground_sprite = _ref_DungeonBoard.get_sprite(
-				_new_MainGroupTag.GROUND, flow_path[i][0], flow_path[i][1])
-		if ground_sprite == null:
-			continue
-		_rotate_sprite(ground_sprite, select_index)
-
-
-func _rotate_sprite(ground_sprite: Sprite, select_index: int) -> void:
-	_ref_SwitchSprite.switch_sprite(
-			ground_sprite, _get_sprite_tag(select_index))
-	_ref_ObjectData.set_state(
-			ground_sprite, _get_state_tag(select_index))
-
-
-func _get_select_index() -> int:
-	return _ref_RandomNumber.get_int(0, _direction_sprite_state.size())
-
-
-func _get_direction(select_index: int) -> Array:
-	return _direction_sprite_state[select_index][0]
-
-
-func _get_sprite_tag(select_index: int) -> String:
-	return _direction_sprite_state[select_index][1]
-
-
-func _get_state_tag(select_index: int) -> String:
-	return _direction_sprite_state[select_index][2]
+func _filter_change_flow(source: Array, index: int, _opt_arg) -> bool:
+	_ref_ObjectData.set_state(source[index], _new_ObjectStateTag.DEFAULT)
+	return true
--- library/npc_data/StyxData.gd
+++ library/npc_data/StyxData.gd
@@ -4,5 +4,4 @@ const LIGHTHOUSE: int = 2
 const HARBOR: int = 1
 const RESTORE_TURN: int = 6
 
-const FLOW_LENGTH: int = 4
-const MAX_FLOW: int = 20
+const FLOW_LENGTH: int = 2
