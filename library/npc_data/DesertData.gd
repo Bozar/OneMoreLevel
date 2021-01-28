3e569c7613c9969ef55f4870023973e92c7b5e55
bozar42 <5583771+Bozar@users.noreply.github.com>
2021-01-28 10:24:56 +0800

    In Desert, collect 4 quality spices, instead of 5, to beat the game.

--- library/npc_data/DesertData.gd
+++ library/npc_data/DesertData.gd
@@ -1,19 +1,19 @@
-const MAX_LENGTH: int = 17
-const MIN_LENGTH: int = 8
+const MAX_LENGTH: int = 20
+const MIN_LENGTH: int = 10
 
 const SPICE_START: int = 2
 const SPICE_END: int = 5
 
 const CREATE_SPICE: int = 15
-const BONUS_CREATE_SPICE: int = 10
-const CREATE_ACTIVE_SPICE: int = 25
+const BONUS_CREATE_SPICE: int = 5
+const CREATE_ACTIVE_SPICE: int = 20
 
 const HP_TURN: int = 1
 const HP_WAIT: int = 5
 const HP_SPICE: int = 10
 const HP_BURY: int = 50
 
-const MAX_SPICE: int = 5
+const MAX_SPICE: int = 4
 const MAX_WORM: int = 3
 const MAX_COOLDOWN: int = 3
 const WORM_DISTANCE: int = 5
