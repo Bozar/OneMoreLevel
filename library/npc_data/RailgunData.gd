0413e92118c0167c8ca3d9eb8b741d40cd962ef3
bozar42 <5583771+Bozar@users.noreply.github.com>
2021-03-08 23:19:12 +0800

    Reduce PC's sight range in Railgun.

--- library/npc_data/RailgunData.gd
+++ library/npc_data/RailgunData.gd
@@ -1,8 +1,8 @@
-const PC_FRONT_SIGHT: int = 8
+const PC_FRONT_SIGHT: int = 6
 const PC_SIDE_SIGHT: int = 4
 const CLOSE_RANGE: int = 1
 
-const NPC_SIGHT: int = 10
+const NPC_SIGHT: int = 8
 const NPC_ATTACK: int = 4
 const NPC_GAP: int = 2
 const MIN_NPC: int = 6
@@ -18,4 +18,4 @@ const COUNTER_WIDTH: int = 3
 const MAX_AMMO: int = 5
 
 const RESTORE_AMMO: int = 1
-const RESTORE_TURN: int = 9
+const RESTORE_TURN: int = 7
