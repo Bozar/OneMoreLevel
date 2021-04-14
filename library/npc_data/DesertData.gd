582fbfeeff0e26b7e810fce6eb7d48f9ac0f1d98
bozar42 <5583771+Bozar@users.noreply.github.com>
2021-04-14 21:18:28 +0800

    Desert: Reduce the time that a sandworm spends on the surface.

--- library/npc_data/DesertData.gd
+++ library/npc_data/DesertData.gd
@@ -3,14 +3,14 @@ const MIN_LENGTH: int = 10
 const SPICE_START: int = 2
 const SPICE_END: int = 5
 
-const CREATE_SPICE: int = 15
+const CREATE_SPICE: int = 10
 const BONUS_CREATE_SPICE: int = 10
 const CREATE_QUALITY_SPICE: int = 20
 
 const HP_TURN: int = 1
 const HP_WAIT: int = 5
 const HP_SPICE: int = 10
-const HP_BURY: int = 50
+const HP_BURY: int = 25
 
 const MAX_SPICE: int = 3
 const MAX_WORM: int = 2
