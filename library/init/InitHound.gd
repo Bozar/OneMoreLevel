5d99e43bb8009adaa474b7a15dd5f529d7f3f8e6
bozar42 <5583771+Bozar@users.noreply.github.com>
2021-04-16 14:58:09 +0800

    Hound: Create walls.

--- library/init/InitHound.gd
+++ library/init/InitHound.gd
@@ -1,10 +1,17 @@
 extends "res://library/init/WorldTemplate.gd"
-# Initialize a simple map for testing.
 
 
-var _spr_Dwarf := preload("res://sprite/Dwarf.tscn")
+const ROOM_SIZE: int = 4
+const CORRIDOR_WIDTH: int = 1
+const PICK_ROOM: int = 11
+const MIN_X: int = 1
+const MIN_Y: int = 0
+const MIN_SHIFT: = 1
+const MAX_SHIFT: = 3
 
-var _dungeon: Dictionary = {}
+var _spr_PCHound := preload("res://sprite/PCHound.tscn")
+
+var _new_HoundData := preload("res://library/npc_data/HoundData.gd").new()
 
 
 func _init(parent_node: Node2D).(parent_node) -> void:
@@ -14,38 +21,52 @@ func _init(parent_node: Node2D).(parent_node) -> void:
 func get_blueprint() -> Array:
 	_init_wall()
 	_init_floor()
-	_init_pc()
-	_init_dwarf()
+	_create_pc()
 
 	return _blueprint
 
 
 func _init_wall() -> void:
-	var shift: int = 2
-	var min_x: int = _new_DungeonSize.CENTER_X - shift
-	var max_x: int = _new_DungeonSize.CENTER_X + shift + 1
-	var min_y: int = _new_DungeonSize.CENTER_Y - shift
-	var max_y: int = _new_DungeonSize.CENTER_Y + shift + 1
-
-	for x in range(min_x, max_x):
-		for y in range(min_y, max_y):
-			_occupy_position(x, y)
-			_add_to_blueprint(_spr_Wall,
-					_new_MainGroupTag.BUILDING, _new_SubGroupTag.WALL, x, y)
-
-
-func _init_dwarf() -> void:
-	var dwarf: int = _ref_RandomNumber.get_int(3, 6)
+	var start_point: Array = _get_top_left_position()
 	var x: int
 	var y: int
+	var neighbor: Array
 
-	while dwarf > 0:
+	_new_ArrayHelper.rand_picker(start_point, PICK_ROOM, _ref_RandomNumber)
+	for i in start_point:
+		x = i[0] + _ref_RandomNumber.get_int(MIN_SHIFT, MAX_SHIFT)
+		y = i[1] + _ref_RandomNumber.get_int(MIN_SHIFT, MAX_SHIFT)
+		neighbor = _new_CoordCalculator.get_neighbor(x, y, 1)
+		for j in neighbor:
+			_occupy_position(j[0], j[1])
+			_add_to_blueprint(_spr_Wall,
+					_new_MainGroupTag.BUILDING, _new_SubGroupTag.WALL,
+					j[0], j[1])
+
+
+func _get_top_left_position() -> Array:
+	var valid_position: Array = []
+	var step: int = ROOM_SIZE + CORRIDOR_WIDTH
+
+	# By setting MIN_X to 1, it is guarantted that every grid is reachable.
+	for x in range(MIN_X, _new_DungeonSize.MAX_X, step):
+		if x + ROOM_SIZE < _new_DungeonSize.MAX_X:
+			for y in range(MIN_Y, _new_DungeonSize.MAX_Y, step):
+				if y + ROOM_SIZE < _new_DungeonSize.MAX_Y:
+					valid_position.push_back([x, y])
+	return valid_position
+
+
+func _create_pc() -> void:
+	var x: int
+	var y: int
+	var neighbor: Array
+
+	while true:
 		x = _ref_RandomNumber.get_x_coord()
 		y = _ref_RandomNumber.get_y_coord()
-		if _is_occupied(x, y):
-			continue
-
-		_occupy_position(x, y)
-		_add_to_blueprint(_spr_Dwarf,
-				_new_MainGroupTag.ACTOR, _new_SubGroupTag.DWARF, x, y)
-		dwarf -= 1
+		neighbor = _new_CoordCalculator.get_neighbor(x, y, 1)
+		for i in neighbor:
+			if not _is_occupied(i[0], i[1]):
+				_init_pc(_new_HoundData.PC_SIGHT, i[0], i[1], _spr_PCHound)
+				return
--- user/doc/hound.md
+++ user/doc/hound.md
@@ -2,4 +2,4 @@
 
 (Press Enter to view key bindings.)
 
-It was a dark and stormy night. I did not speak with my friend since afternoon because he criticized my previous work, A Study in Emerald, as a thriller by an untalented writer. As for now, he was quietly investigating a curious wooden figurine from Mycroft. "I don't think a man with an octopus head shaves his tentacles, my dear doctor." He put the figurine down and gave me an opened envelop. "On the other hand, can you read this letter for me? It's from the Baskervilles."
+> It was a dark and stormy night. I did not speak with my friend since afternoon because he criticized my previous work, A Study in Emerald, as a thriller by an untalented writer. As for now, he was quietly investigating a curious wooden figurine from Mycroft. "I don't think a man with an octopus head shaves his tentacles, my dear doctor." He put the figurine down and gave me an opened envelop. "On the other hand, can you read this letter for me? It's from the Baskervilles."
