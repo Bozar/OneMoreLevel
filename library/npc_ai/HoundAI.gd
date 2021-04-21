9ad71bcb45696cd80107e51c5cd44303a1015688
bozar42 <5583771+Bozar@users.noreply.github.com>
2021-04-21 11:02:53 +0800

    Hound: Count down boss duration.

--- library/npc_ai/HoundAI.gd
+++ library/npc_ai/HoundAI.gd
@@ -1,11 +1,23 @@
 extends "res://library/npc_ai/AITemplate.gd"
 
 
+const INIT_DURATION: int = -1
+
+var _new_HoundData := preload("res://library/npc_data/HoundData.gd").new()
+
+var _boss_duration: int = INIT_DURATION
+var _boss_escape: int = 0
+
+
 func _init(parent_node: Node2D).(parent_node) -> void:
 	pass
 
 
 func take_action() -> void:
+	if _self.is_in_group(_new_SubGroupTag.HOUND_BOSS):
+		if not _boss_countdown():
+			return
+
 	_switch_sprite()
 
 
@@ -25,3 +37,28 @@ func _switch_sprite() -> void:
 	else:
 		sprite_type = _new_SpriteTypeTag.DEFAULT
 	_ref_SwitchSprite.switch_sprite(_self, sprite_type)
+
+
+func _boss_countdown() -> bool:
+	var boss_to_pc: int
+	var extra_duration: int
+
+	if _boss_duration == INIT_DURATION:
+		boss_to_pc = _new_CoordCalculator.get_range(_self_pos[0], _self_pos[1],
+				_pc_pos[0], _pc_pos[1])
+		extra_duration = _ref_RandomNumber.get_int(
+				_new_HoundData.MIN_BOSS_DURATION,
+				_new_HoundData.MAX_BOSS_DURATION)
+		_boss_duration = boss_to_pc + extra_duration
+
+	if _boss_duration > 0:
+		_boss_duration -= 1
+		return true
+	else:
+		_boss_duration = INIT_DURATION
+		_boss_escape += 1
+		_ref_RemoveObject.remove(_new_MainGroupTag.ACTOR,
+				_self_pos[0], _self_pos[1])
+		if _boss_escape == _new_HoundData.MAX_BOSS_ESCAPE:
+			_ref_EndGame.player_win()
+		return false
--- library/npc_data/HoundData.gd
+++ library/npc_data/HoundData.gd
@@ -8,9 +8,12 @@ const FOG_DURATION: int = 3
 const MIN_HOUND_GAP: int = 3
 const MAX_HOUND: int = 10
 const MAX_BOSS_HIT_POINT: int = 3
+const MAX_BOSS_ESCAPE: int = 3
 
 const START_RESPAWN: int = 6
 const MIN_MINION_DISTANCE: int = 5
 const MAX_MINION_DISTANCE: int = 10
 const MIN_BOSS_DISTANCE: int = 10
 const MAX_BOSS_DISTANCE: int = 15
+const MIN_BOSS_DURATION: int = 5
+const MAX_BOSS_DURATION: int = 11
