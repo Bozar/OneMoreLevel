d7aae7c94f8cc0d0d70d4df4e7d11b3bb70dc061
bozar42 <5583771+Bozar@users.noreply.github.com>
2021-04-28 15:03:40 +0800

    Parse a color palette json file.

--- scene/main/Palette.gd
+++ scene/main/Palette.gd
@@ -1,6 +1,7 @@
 extends Node2D
 class_name Game_Palette
 
+
 const MAIN_GROUP_TAG := preload("res://library/MainGroupTag.gd")
 const SUB_GROUP_TAG := preload("res://library/SubGroupTag.gd")
 
@@ -11,13 +12,17 @@ const GROUND: String = "ground"
 const TRAP: String = "trap"
 const BUILDING: String = "building"
 const ACTOR: String = "actor"
-const GUI_TEXT: String = "text"
+const GUI_TEXT: String = "gui_text"
 
 const DARK_GROUND: String = "dark_ground"
 const DARK_TRAP: String = "dark_trap"
 const DARK_BUILDING: String = "dark_building"
 const DARK_ACTOR: String = "dark_actor"
-const DARK_GUI_TEXT: String = "dark_text"
+const DARK_GUI_TEXT: String = "dark_gui_text"
+
+const COLOR_VALUE: String = "color_value"
+
+const HTML_COLOR_REGEX: String = "^#{0,1}([0-9A-Fa-f]{3}){1,2}$"
 
 # https://coolors.co/f8f9fa-e9ecef-dee2e6-ced4da-adb5bd-6c757d-495057-343a40-212529
 const BLACK: String = "#212529"
@@ -35,7 +40,7 @@ const DARK_ORANGE: String = "#854E19"
 
 const DEBUG: String = "#FE4A49"
 
-const TAG_TO_COLOR: Dictionary = {
+const DEFAULT_TAG_TO_COLOR: Dictionary = {
 	BACKGROUND: BLACK,
 	INDICATOR: GREY,
 
@@ -71,42 +76,43 @@ const SUB_TAG_TO_COLOR: Dictionary = {
 	SUB_GROUP_TAG.PC_MIRROR_IMAGE: DARK_ACTOR,
 }
 
-
 var _ref_GameSetting: Game_GameSetting
+
+var _tag_to_color: Dictionary = {}
 
 
 func get_default_color(main_tag: String, sub_tag: String = "") -> String:
 	if SUB_TAG_TO_COLOR.has(sub_tag):
-		return TAG_TO_COLOR[SUB_TAG_TO_COLOR[sub_tag]]
+		return _tag_to_color[SUB_TAG_TO_COLOR[sub_tag]]
 	else:
 		match main_tag:
 			MAIN_GROUP_TAG.GROUND:
-				return TAG_TO_COLOR[GROUND]
+				return _tag_to_color[GROUND]
 			MAIN_GROUP_TAG.TRAP:
-				return TAG_TO_COLOR[TRAP]
+				return _tag_to_color[TRAP]
 			MAIN_GROUP_TAG.BUILDING:
-				return TAG_TO_COLOR[BUILDING]
+				return _tag_to_color[BUILDING]
 			MAIN_GROUP_TAG.ACTOR:
-				return TAG_TO_COLOR[ACTOR]
+				return _tag_to_color[ACTOR]
 			MAIN_GROUP_TAG.INDICATOR:
-				return TAG_TO_COLOR[INDICATOR]
+				return _tag_to_color[INDICATOR]
 			_:
 				return DEBUG
 
 
 func get_dark_color(main_tag: String, sub_tag: String = "") -> String:
 	if SUB_TAG_TO_COLOR.has(sub_tag):
-		return TAG_TO_COLOR[SUB_TAG_TO_COLOR[sub_tag]]
+		return _tag_to_color[SUB_TAG_TO_COLOR[sub_tag]]
 	else:
 		match main_tag:
 			MAIN_GROUP_TAG.GROUND:
-				return TAG_TO_COLOR[DARK_GROUND]
+				return _tag_to_color[DARK_GROUND]
 			MAIN_GROUP_TAG.TRAP:
-				return TAG_TO_COLOR[DARK_TRAP]
+				return _tag_to_color[DARK_TRAP]
 			MAIN_GROUP_TAG.BUILDING:
-				return TAG_TO_COLOR[DARK_BUILDING]
+				return _tag_to_color[DARK_BUILDING]
 			MAIN_GROUP_TAG.ACTOR:
-				return TAG_TO_COLOR[DARK_ACTOR]
+				return _tag_to_color[DARK_ACTOR]
 			_:
 				return DEBUG
 
@@ -125,11 +131,32 @@ func set_dark_color(set_sprite: Sprite, main_tag: String,
 
 func get_text_color(is_light_color: bool) -> String:
 	if is_light_color:
-		return TAG_TO_COLOR[GUI_TEXT]
-	return TAG_TO_COLOR[DARK_GUI_TEXT]
+		return _tag_to_color[GUI_TEXT]
+	return _tag_to_color[DARK_GUI_TEXT]
 
 
 func _on_GameSetting_setting_loaded() -> void:
-	# var palette: Dictionary = _ref_GameSetting.get_palette()
-	# VisualServer.set_default_clear_color(palette["color_value"]["black"])
-	pass
+	var palette: Dictionary = _ref_GameSetting.get_palette()
+	var has_color_value: bool = palette.has(COLOR_VALUE) \
+			and (palette[COLOR_VALUE] is Dictionary)
+	var color_regex := RegEx.new()
+	var __ = color_regex.compile(HTML_COLOR_REGEX)
+
+	for i in DEFAULT_TAG_TO_COLOR.keys():
+		_tag_to_color[i] = ""
+		if _dict_has_string(palette, i):
+			if has_color_value \
+					and _dict_has_string(palette[COLOR_VALUE], palette[i]):
+				_tag_to_color[i] = palette[COLOR_VALUE][palette[i]]
+			else:
+				_tag_to_color[i] = palette[i]
+
+		if color_regex.search(_tag_to_color[i]) == null:
+			# print("Invalid color: " + i)
+			_tag_to_color[i] =  DEFAULT_TAG_TO_COLOR[i]
+
+	VisualServer.set_default_clear_color(_tag_to_color[BACKGROUND])
+
+
+func _dict_has_string(this_dict: Dictionary, this_key: String) -> bool:
+	return this_dict.has(this_key) and (this_dict[this_key] is String)
