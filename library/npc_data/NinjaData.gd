d34b120815b17f49c672cacdb8a7664de887faae
bozar42 <5583771+Bozar@users.noreply.github.com>
2021-05-25 14:38:54 +0800

    Ninja: Redesign dungeon map.

--- library/npc_data/NinjaData.gd
+++ library/npc_data/NinjaData.gd
@@ -3,8 +3,8 @@ class_name Game_NinjaData
 
 const PC_SIGHT: int = 6
 const MAX_TIME_STOP: int = 9
-const TIME_STOP_DIVISOR: float = 10.0
-const MAX_SOUL_DURATION: int = 10
+const TIME_STOP_DIVISOR: float = 8.0
+const MAX_SOUL_DURATION: int = 8
 
 const MAX_NPC: int = 12
 const MIN_DISTANCE_TO_PC: int = 5
