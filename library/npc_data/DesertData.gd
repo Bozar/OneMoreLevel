dfb3ef6d03b4d35c58fada1a6c20c83c181e25e7
bozar42 <5583771+Bozar@users.noreply.github.com>
2022-06-28 10:42:19 +0800

    Desert: Tweak difficulty.

--- library/npc_data/DesertData.gd
+++ library/npc_data/DesertData.gd
@@ -6,14 +6,16 @@ const MIN_LENGTH := 12
 const SPICE_START := 2
 const SPICE_END := 5
 
-const CREATE_SPICE := 10
+# const CREATE_SPICE := 10
+const CREATE_SPICE := 5
 const BONUS_CREATE_SPICE := 5
 const CREATE_QUALITY_SPICE := 20
 
 const HP_TURN := 1
 const HP_WAIT := 5
 const HP_SPICE := 10
-const HP_BURY := 35
+# const HP_BURY := 35
+const HP_BURY := 30
 
 const MAX_SPICE := 3
 const MAX_WORM := 2
