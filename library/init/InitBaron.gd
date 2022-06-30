0ce4fa813f1c94e6b1d01aca2b0db69ccc965902
bozar42 <5583771+Bozar@users.noreply.github.com>
2022-06-30 10:13:49 +0800

    Baron: Create bandits.

--- library/init/InitBaron.gd
+++ library/init/InitBaron.gd
@@ -9,7 +9,7 @@ const Y_STEP := 5
 const CHAR_TRUNK := "O"
 const CHAR_BRANCH := "+"
 
-enum TERRAIN {
+enum {
 	BRANCH = DEFAULT_MARKER + 1, TRUNK, BANDIT, BIRD,
 }
 
@@ -17,12 +17,7 @@ var _spr_TreeTrunk := preload("res://sprite/TreeTrunk.tscn")
 var _spr_TreeBranch := preload("res://sprite/TreeBranch.tscn")
 var _spr_PCBaron := preload("res://sprite/PCBaron.tscn")
 var _spr_Bird := preload("res://sprite/Bird.tscn")
-
-var _char_to_blueprint := {
-	CHAR_TRUNK: [_spr_TreeTrunk, Game_SubTag.TREE_TRUNK, TERRAIN.TRUNK],
-	CHAR_BRANCH: [_spr_TreeBranch, Game_SubTag.TREE_BRANCH, TERRAIN.BRANCH],
-}
-var _tree_grids := []
+var _spr_Bandit := preload("res://sprite/Bandit.tscn")
 
 
 func _init(parent_node: Node2D).(parent_node) -> void:
@@ -30,44 +25,27 @@ func _init(parent_node: Node2D).(parent_node) -> void:
 
 
 func get_blueprint() -> Array:
-	_create_tree()
-	_create_floor()
+	var terrain_map := _create_tree_floor()
 
-	Game_ArrayHelper.rand_picker(_tree_grids, Game_BaronData.MAX_BIRD + 1,
-			_ref_RandomNumber)
-	_create_pc()
-	_create_bird()
-	_tree_grids.clear()
+	_create_pc(terrain_map[BIRD])
+	_create_bird(terrain_map[BIRD])
+	_create_bandit(terrain_map[BANDIT])
 
 	return _blueprint
 
 
-func _create_tree() -> void:
+func _create_tree_floor() -> Dictionary:
 	var packed_prefabs: Array = _load_prefabs()
 	var prefab_index := 0
 	var this_prefab: Game_DungeonPrefab.PackedPrefab
-	var this_char: String
-	var this_x: int
-	var this_y: int
+	var terrain := {BIRD: [], BANDIT: [],}
 
 	for start_x in range(0, Game_DungeonSize.MAX_X, X_STEP):
 		for start_y in range(0, Game_DungeonSize.MAX_Y, Y_STEP):
 			this_prefab = packed_prefabs[prefab_index]
 			prefab_index += 1
-
-			for x in this_prefab.max_x:
-				for y in this_prefab.max_y:
-					this_char = this_prefab.prefab[x][y]
-					this_x = x + start_x
-					this_y = y + start_y
-					if _char_to_blueprint.has(this_char):
-						_add_building_to_blueprint(
-								_char_to_blueprint[this_char][0],
-								_char_to_blueprint[this_char][1],
-								this_x, this_y)
-						_set_terrain_marker(this_x, this_y,
-								_char_to_blueprint[this_char][2])
-						_tree_grids.push_back(Game_IntCoord.new(this_x, this_y))
+			_parse_prefab(this_prefab, start_x, start_y, terrain)
+	return terrain
 
 
 func _load_prefabs() -> Array:
@@ -78,6 +56,7 @@ func _load_prefabs() -> Array:
 	# There must be more than MAX_PREFAB prefabs.
 	Game_ArrayHelper.rand_picker(file_list, MAX_PREFAB, _ref_RandomNumber)
 	for i in file_list:
+		# print(i)
 		edit_arg = [
 			Game_DungeonPrefab.HORIZONTAL_FLIP \
 					if _ref_RandomNumber.get_percent_chance(FLIP_CHANCE) \
@@ -91,22 +70,65 @@ func _load_prefabs() -> Array:
 	return packed_prefabs
 
 
-func _create_floor() -> void:
-	for x in range(Game_DungeonSize.MAX_X):
-		for y in range(Game_DungeonSize.MAX_Y):
-			if _get_terrain_marker(x, y) > DEFAULT_MARKER:
-				continue
-			_add_ground_to_blueprint(_spr_Floor, Game_SubTag.FLOOR, x, y)
+func _parse_prefab(this_prefab: Game_DungeonPrefab.PackedPrefab,
+		start_x: int, start_y: int, terrain: Dictionary) -> void:
+	var this_char: String
+	var this_x: int
+	var this_y: int
+	var is_tree: bool
+	var is_ground: bool
+	var grounds := []
+
+	for x in this_prefab.max_x:
+		for y in this_prefab.max_y:
+			this_char = this_prefab.prefab[x][y]
+			this_x = x + start_x
+			this_y = y + start_y
+			is_tree = true
+			is_ground = true
+
+			match this_char:
+				CHAR_TRUNK:
+					_add_building_to_blueprint(_spr_TreeTrunk,
+							Game_SubTag.TREE_TRUNK, this_x, this_y)
+					_set_terrain_marker(this_x, this_y, TRUNK)
+					is_ground = false
+				CHAR_BRANCH:
+					_add_building_to_blueprint(_spr_TreeBranch,
+							Game_SubTag.TREE_BRANCH, this_x, this_y)
+					_set_terrain_marker(this_x, this_y, BRANCH)
+				_:
+					_add_ground_to_blueprint(_spr_Floor,
+							Game_SubTag.FLOOR, this_x, this_y)
+					is_tree = false
+
+			if is_tree:
+				terrain[BIRD].push_back(Game_IntCoord.new(this_x, this_y))
+			if is_ground:
+				grounds.push_back(Game_IntCoord.new(this_x, this_y))
+
+	Game_ArrayHelper.shuffle(grounds, _ref_RandomNumber)
+	terrain[BANDIT].push_back(grounds.pop_back())
 
 
-func _create_pc() -> void:
-	var pc_coord: Game_IntCoord = _tree_grids.pop_back()
+func _create_pc(terrain_map: Array) -> void:
+	var pc_pos: Game_IntCoord
 
-	_add_actor_to_blueprint(_spr_PCBaron, Game_SubTag.PC,
-			pc_coord.x, pc_coord.y, Game_BaronData.TREE_LAYER)
+	Game_ArrayHelper.shuffle(terrain_map, _ref_RandomNumber)
+	pc_pos = terrain_map.pop_back()
+	_add_actor_to_blueprint(_spr_PCBaron, Game_SubTag.PC, pc_pos.x, pc_pos.y,
+			Game_BaronData.TREE_LAYER)
 
 
-func _create_bird() -> void:
-	for i in _tree_grids:
+func _create_bird(terrain_map: Array) -> void:
+	terrain_map.resize(Game_BaronData.MAX_BIRD)
+	for i in terrain_map:
 		_add_actor_to_blueprint(_spr_Bird, Game_SubTag.BIRD, i.x, i.y,
 				Game_BaronData.TREE_LAYER)
+
+
+func _create_bandit(terrain_map: Array) -> void:
+	Game_ArrayHelper.rand_picker(terrain_map, Game_BaronData.MAX_BANDIT,
+			_ref_RandomNumber)
+	for i in terrain_map:
+		_add_actor_to_blueprint(_spr_Bandit, Game_SubTag.BANDIT, i.x, i.y)
